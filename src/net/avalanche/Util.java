991bb2482ac7b33d91e1a5612db85a54268debe4
Paul Reilly <paldiutheminecrafter@gmail.com>
2013-10-26 23:46:17 -0400

    Updated Util for Stopcommand

--- src/net/avalanche/Util.java
+++ src/net/avalanche/Util.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 public class Util
 {
     protected Main plugin;
+    public static final List<String> STOP_COMMANDS = Arrays.asList("stop", "off", "end", "halt", "die");
     
     public static void bcastMsg(String message, ChatColor color)
     {
@@ -20,6 +21,11 @@ public class Util
     public static void bcastMsg(String message)
     {
         Util.bcastMsg(message, null);
+    }
+    
+    public static boolean isStopCommand(String command)
+    {
+        return STOP_COMMANDS.contains(command.toLowerCase());
     }
     
     public static String colorise(String string)

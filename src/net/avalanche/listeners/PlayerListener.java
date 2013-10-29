3522623832e8bf0c8d59b6f405b2fc32708e757b
Paul Reilly <paldiutheminecrafter@gmail.com>
2013-10-28 23:45:07 -0400

    Moved event to listener; Fixed formatting issues.

--- src/net/avalanche/listeners/PlayerListener.java
+++ src/net/avalanche/listeners/PlayerListener.java
@@ -5,5 +5,65 @@ import org.bukkit.event.Listener;
 
 public class PlayerListener implements Listener
 {
-    public Main plugin;
+    protected Main plugin;
+    
+    @EventHandler
+    public void onChat(AsyncPlayerChatEvent e)
+    {
+    	Player p = e.getPlayer();
+    	if(this.getConfig().getConfigurationSection("Chat").getString("ChatEnabled").equals("true"))
+    	{
+    	    if (p.hasPermission("avalanche.chat.priority.1"))
+    	    {
+    		    e.setFormat(ChatColor.translateAlternateColorCodes('&', this.getConfig().getConfigurationSection("Chat").getString("Priority1")).replaceAll("%displayname%", p.getDisplayName()).replaceAll("%message%", e.getMessage()));
+    	    }
+    		else if (p.hasPermission("avalanche.chat.priority.2"))
+    		{
+        	    e.setFormat(ChatColor.translateAlternateColorCodes('&', this.getConfig().getConfigurationSection("Chat").getString("Priority2")).replaceAll("%displayname%", p.getDisplayName()).replaceAll("%message%", e.getMessage()));
+    	    }
+    		else if (p.hasPermission("avalanche.chat.priority.3"))
+    		{
+        		e.setFormat(ChatColor.translateAlternateColorCodes('&', this.getConfig().getConfigurationSection("Chat").getString("Priority3")).replaceAll("%displayname%", p.getDisplayName()).replaceAll("%message%", e.getMessage()));
+    	    }
+    		else if (p.hasPermission("avalanche.chat.priority.4"))
+    		{
+        		e.setFormat(ChatColor.translateAlternateColorCodes('&', this.getConfig().getConfigurationSection("Chat").getString("Priority4")).replaceAll("%displayname%", p.getDisplayName()).replaceAll("%message%", e.getMessage()));
+    	    }
+    		else if (p.hasPermission("avalanche.chat.priority.5"))
+    		{
+        		e.setFormat(ChatColor.translateAlternateColorCodes('&', this.getConfig().getConfigurationSection("Chat").getString("Priority5")).replaceAll("%displayname%", p.getDisplayName()).replaceAll("%message%", e.getMessage()));
+    	    }
+    		else if (p.hasPermission("avalanche.chat.priority.6"))
+    		{
+        		e.setFormat(ChatColor.translateAlternateColorCodes('&', this.getConfig().getConfigurationSection("Chat").getString("Priority6")).replaceAll("%displayname%", p.getDisplayName()).replaceAll("%message%", e.getMessage()));
+    	    }
+    		else if (p.hasPermission("avalanche.chat.priority.7"))
+    		{
+        		e.setFormat(ChatColor.translateAlternateColorCodes('&', this.getConfig().getConfigurationSection("Chat").getString("Priority7")).replaceAll("%displayname%", p.getDisplayName()).replaceAll("%message%", e.getMessage()));
+    		}
+    		else if (p.hasPermission("avalanche.chat.priority.8"))
+    		{
+        		e.setFormat(ChatColor.translateAlternateColorCodes('&', this.getConfig().getConfigurationSection("Chat").getString("Priority8")).replaceAll("%displayname%", p.getDisplayName()).replaceAll("%message%", e.getMessage()));
+    	    }
+    		else if (p.hasPermission("avalanche.chat.priority.9"))
+    		{
+        		e.setFormat(ChatColor.translateAlternateColorCodes('&', this.getConfig().getConfigurationSection("Chat").getString("Priority9")).replaceAll("%displayname%", p.getDisplayName()).replaceAll("%message%", e.getMessage()));
+    		}
+    		else if(p.hasPermission("avalanche.chat.priority.10"))
+    		{
+        		e.setFormat(ChatColor.translateAlternateColorCodes('&', this.getConfig().getConfigurationSection("Chat").getString("Priority10")).replaceAll("%displayname%", p.getDisplayName()).replaceAll("%message%", e.getMessage()));
+    	    }
+    	}
+    	
+    	if (this.getConfig().getConfigurationSection("Chat").getString("AmIAwesome").equals("true"))
+    	{
+			if (p.getName().equalsIgnoreCase("freelix2000") || p.getName().equalsIgnoreCase("paldiu"))
+			{
+				e.setFormat(ChatColor.DARK_GRAY + "[" + ChatColor.AQUA + "Dev" + ChatColor.DARK_GRAY + "]" + p.getDisplayName() + ChatColor.GRAY + ": " + ChatColor.AQUA + e.getMessage());
+			}
+			else
+			{
+			}
+		}
+    }
 }
